268778282b4d852b57aabd47b0fb3cc96476e539
jilong.zhang <jilong.zhang@zhaopin.com.cn>
2018-03-07 15:18:41 +0800

    Hystrix移至ribbon,删除Hystrix

--- eurekaribbon/src/main/java/com/example/eurekaribbon/EurekaribbonApplication.java
+++ eurekaribbon/src/main/java/com/example/eurekaribbon/EurekaribbonApplication.java
@@ -2,9 +2,15 @@ package com.example.eurekaribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableDiscoveryClient
+@EnableEurekaClient
+@EnableCircuitBreaker//EnableHystrix 等价
+@EnableHystrixDashboard
 @SpringBootApplication
 public class EurekaribbonApplication {
 
--- eurekaribbon/src/main/java/com/example/eurekaribbon/service/HelloService.java
+++ eurekaribbon/src/main/java/com/example/eurekaribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.example.eurekaribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
@@ -15,9 +16,14 @@ public class HelloService {
     @Autowired
     LoadBalancerClient loadBalancerClient;
 
+    @HystrixCommand(fallbackMethod = "fallback")
     public String sayHello(String name){
         ServiceInstance serviceInstance = loadBalancerClient.choose("EUREKASERVERPROVIDER");
         System.out.println(serviceInstance.getHost() + ":" + serviceInstance.getPort()+"---------------------------");
         return  restTemplate.getForObject("http://EUREKASERVERPROVIDER/name/"+name,String.class);
+    }
+
+    public String fallback(String name) {
+        return "some exception occur call fallback method.";
     }
 }
