6dcaa7136811813a903f63d6ebf5e4ec0118fbf2
Melo15Zhang <36220900+melo15zhang@users.noreply.github.com>
2019-09-23 16:08:30 +0800

    v1.2

--- configserver/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ configserver/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -8,9 +8,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigserverApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigserverApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
